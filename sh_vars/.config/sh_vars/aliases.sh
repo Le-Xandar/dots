d73d6cdadf94600acaf0778cd83effbd1d49fac7
westofer <westoferRaymond@gmail.com>
2021-02-18 13:42:23 +0300

    stuff sh_vars yada yad y

---
+++ sh_vars/.config/sh_vars/aliases.sh
@@ -0,0 +1,20 @@
+alias fm='ranger'
+alias fm.='. ranger'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias sl='ls -CF'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias update='sudo pacman -Sy && sudo powerpill -Su && paru -Su'
+
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
